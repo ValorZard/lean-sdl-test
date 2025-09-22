87b66e754cace24c5ca9503789cca65ee616c0a8
Srayan Jana <valorzard.kiko@outlook.com>
2025-09-21 18:52:37 -0700

    default target

--- lakefile.lean
+++ lakefile.lean
@@ -6,10 +6,10 @@ package «lean-sdl-test» where
 
 require SDL3 from git "https://github.com/ValorZard/lean-sdl3.git" @ "master"
 
-@[default_target]
 lean_lib LeanSdlTest where
   -- Add any library-specific configurations here
 
+@[default_target]
 lean_exe «lean-sdl-test» where
   root := `Main
   -- Link with SDL libraries using full paths to the import libraries
