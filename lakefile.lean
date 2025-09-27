1898ba32ecc4110f00bdb863b0a95833b01b07e2
Srayan Jana <valorzard.kiko@outlook.com>
2025-09-26 21:41:05 -0700

    copy assets to make game distributable

--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,18 @@ require SDL3 from git "https://github.com/ValorZard/lean-sdl3.git" @ "master"
 lean_lib LeanSdlTest where
   -- Add any library-specific configurations here
 
+target copyAssets : Unit := Job.async do
+  let dstDir := ((<- getRootPackage).binDir) / "assets"
+  IO.FS.createDirAll dstDir
+  logInfo s!"Copying binaries from assets to {dstDir}"
+  let assetsDir : System.FilePath := (<- getRootPackage).dir / "assets"
+  for entry in (← assetsDir.readDir) do
+    if entry.path.extension != none then
+      copyFile entry.path (dstDir / entry.path.fileName.get!)
+  pure ()
+
 @[default_target]
 lean_exe «lean-sdl-test» where
   root := `Main
+  needs := #[copyAssets]
   moreLinkArgs := if !System.Platform.isWindows then #["-Wl,--allow-shlib-undefined", "-Wl,-rpath=$ORIGIN"] else #[]
