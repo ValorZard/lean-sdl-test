8c2f6ff08a4925c814149c9526e8fb88e83482d9
Srayan Jana <valorzard.kiko@outlook.com>
2025-09-28 16:02:27 -0700

    recursive asset copying

--- lakefile.lean
+++ lakefile.lean
@@ -9,15 +9,21 @@ require SDL3 from git "https://github.com/ValorZard/lean-sdl3.git" @ "master"
 lean_lib LeanSdlTest where
   -- Add any library-specific configurations here
 
+partial def copyAsset (src : System.FilePath) (dst : System.FilePath) : IO Unit := do
+  for entry in (← src.readDir) do
+    if entry.path.extension != none then
+      copyFile entry.path (dst / entry.path.fileName.get!)
+    else
+      IO.FS.createDirAll (dst / entry.path.fileName.get!)
+      copyAsset entry.path (dst / entry.path.fileName.get!)
+  return ()
+
 target copyAssets : Unit := Job.async do
   let dstDir := ((<- getRootPackage).binDir) / "assets"
   IO.FS.createDirAll dstDir
   let assetsDir : System.FilePath := (<- getRootPackage).dir / "assets"
   logInfo s!"Copying assets from {assetsDir} to {dstDir}"
-  for entry in (← assetsDir.readDir) do
-    if entry.path.extension != none then
-      copyFile entry.path (dstDir / entry.path.fileName.get!)
-  pure ()
+  copyAsset assetsDir dstDir
 
 @[default_target]
 lean_exe «lean-sdl-test» where
